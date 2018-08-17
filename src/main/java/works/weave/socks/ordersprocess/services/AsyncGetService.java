b75e4d6b7996c909a11a5a25789f087bcdefe1a4
Daniel Quack <daniel.q@posteo.de>
2018-08-17 12:53:11 +0200

    implement example get to orders (working)

--- src/main/java/works/weave/socks/ordersprocess/services/AsyncGetService.java
+++ src/main/java/works/weave/socks/ordersprocess/services/AsyncGetService.java
@@ -1,9 +1,13 @@
 package works.weave.socks.ordersprocess.services;
 
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import static org.springframework.hateoas.MediaTypes.HAL_JSON;
 
-import works.weave.socks.ordersprocess.config.RestProxyTemplate;
+import java.io.IOException;
+import java.net.URI;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.Future;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -22,74 +26,79 @@ import org.springframework.scheduling.annotation.AsyncResult;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-import java.io.IOException;
-import java.net.URI;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.concurrent.Future;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
-import static org.springframework.hateoas.MediaTypes.HAL_JSON;
+import works.weave.socks.ordersprocess.config.RestProxyTemplate;
 
 @Service
 public class AsyncGetService {
-    private final Logger LOG = LoggerFactory.getLogger(getClass());
+	private final Logger LOG = LoggerFactory.getLogger(getClass());
 
-    private final RestProxyTemplate restProxyTemplate;
+	private final RestProxyTemplate restProxyTemplate;
 
-    private final RestTemplate halTemplate;
+	private final RestTemplate halTemplate;
 
-    @Autowired
-    public AsyncGetService(RestProxyTemplate restProxyTemplate) {
-        this.restProxyTemplate = restProxyTemplate;
-        this.halTemplate = new RestTemplate(restProxyTemplate.getRestTemplate().getRequestFactory());
+	@Autowired
+	public AsyncGetService(RestProxyTemplate restProxyTemplate) {
+		this.restProxyTemplate = restProxyTemplate;
+		this.halTemplate = new RestTemplate(restProxyTemplate.getRestTemplate().getRequestFactory());
 
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-        objectMapper.registerModule(new Jackson2HalModule());
-        MappingJackson2HttpMessageConverter halConverter = new MappingJackson2HttpMessageConverter();
-        halConverter.setSupportedMediaTypes(Arrays.asList(MediaTypes.HAL_JSON));
-        halConverter.setObjectMapper(objectMapper);
-        halTemplate.setMessageConverters(Collections.singletonList(halConverter));
-    }
+		ObjectMapper objectMapper = new ObjectMapper();
+		objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+		objectMapper.registerModule(new Jackson2HalModule());
+		MappingJackson2HttpMessageConverter halConverter = new MappingJackson2HttpMessageConverter();
+		halConverter.setSupportedMediaTypes(Arrays.asList(MediaTypes.HAL_JSON));
+		halConverter.setObjectMapper(objectMapper);
+		halTemplate.setMessageConverters(Collections.singletonList(halConverter));
+	}
 
-    @Async
-    public <T> Future<Resource<T>> getResource(URI url, TypeReferences.ResourceType<T> type) throws
-            InterruptedException, IOException {
-        RequestEntity<Void> request = RequestEntity.get(url).accept(HAL_JSON).build();
-        LOG.debug("Requesting: " + request.toString());
-        Resource<T> body = restProxyTemplate.getRestTemplate().exchange(request, type).getBody();
-        LOG.debug("Received: " + body.toString());
-        return new AsyncResult<>(body);
-    }
+	@Async
+	public <T> Future<Resource<T>> getResource(URI url, TypeReferences.ResourceType<T> type)
+			throws InterruptedException, IOException {
+		RequestEntity<Void> request = RequestEntity.get(url).accept(HAL_JSON).build();
+		LOG.debug("Requesting: " + request.toString());
+		Resource<T> body = restProxyTemplate.getRestTemplate().exchange(request, type).getBody();
+		LOG.debug("Received: " + body.toString());
+		return new AsyncResult<>(body);
+	}
 
-    @Async
-    public <T> Future<Resources<T>> getDataList(URI url, TypeReferences.ResourcesType<T> type) throws
-            InterruptedException, IOException {
-        RequestEntity<Void> request = RequestEntity.get(url).accept(HAL_JSON).build();
-        LOG.debug("Requesting: " + request.toString());
-        Resources<T> body = restProxyTemplate.getRestTemplate().exchange(request, type).getBody();
-        LOG.debug("Received: " + body.toString());
-        return new AsyncResult<>(body);
-    }
+	@Async
+	public String getAmount(URI url) {
+		RestTemplate restTemplate = new RestTemplate();
+		LOG.info("jetzt gehts los");
+		String result = restTemplate.getForObject(url, String.class);
+		LOG.info("Received: " + result);
+		return result;
+	}
 
-    @Async
-    public <T> Future<List<T>> getDataList(URI url, ParameterizedTypeReference<List<T>> type) throws
-            InterruptedException, IOException {
-        RequestEntity<Void> request = RequestEntity.get(url).accept(MediaType.APPLICATION_JSON).build();
-        LOG.debug("Requesting: " + request.toString());
-        List<T> body = restProxyTemplate.getRestTemplate().exchange(request, type).getBody();
-        LOG.debug("Received: " + body.toString());
-        return new AsyncResult<>(body);
-    }
+	@Async
+	public <T> Future<Resources<T>> getDataList(URI url, TypeReferences.ResourcesType<T> type)
+			throws InterruptedException, IOException {
+		RequestEntity<Void> request = RequestEntity.get(url).accept(HAL_JSON).build();
+		LOG.debug("Requesting: " + request.toString());
+		Resources<T> body = restProxyTemplate.getRestTemplate().exchange(request, type).getBody();
+		LOG.debug("Received: " + body.toString());
+		return new AsyncResult<>(body);
+	}
 
-    @Async
-    public <T, B> Future<T> postResource(URI uri, B body, ParameterizedTypeReference<T> returnType) {
-        RequestEntity<B> request = RequestEntity.post(uri).contentType(MediaType.APPLICATION_JSON).accept(MediaType
-                .APPLICATION_JSON).body(body);
-        LOG.debug("Requesting: " + request.toString());
-        T responseBody = restProxyTemplate.getRestTemplate().exchange(request, returnType).getBody();
-        LOG.debug("Received: " + responseBody);
-        return new AsyncResult<>(responseBody);
-    }
+	@Async
+	public <T> Future<List<T>> getDataList(URI url, ParameterizedTypeReference<List<T>> type)
+			throws InterruptedException, IOException {
+		RequestEntity<Void> request = RequestEntity.get(url).accept(MediaType.APPLICATION_JSON).build();
+		LOG.debug("Requesting: " + request.toString());
+		List<T> body = restProxyTemplate.getRestTemplate().exchange(request, type).getBody();
+		LOG.debug("Received: " + body.toString());
+		return new AsyncResult<>(body);
+	}
+
+	@Async
+	public <T, B> Future<T> postResource(URI uri, B body, ParameterizedTypeReference<T> returnType) {
+		RequestEntity<B> request = RequestEntity.post(uri).contentType(MediaType.APPLICATION_JSON)
+				.accept(MediaType.APPLICATION_JSON).body(body);
+		LOG.debug("Requesting: " + request.toString());
+		T responseBody = restProxyTemplate.getRestTemplate().exchange(request, returnType).getBody();
+		LOG.debug("Received: " + responseBody);
+		return new AsyncResult<>(responseBody);
+	}
 }
